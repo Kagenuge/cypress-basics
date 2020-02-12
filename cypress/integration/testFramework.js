129a31fb951d5083df80bf6bf4f31fc89fb4245b
Kagenuge <kaarlen@gmail.com>
2020-02-12 16:04:47 +0200

    form validation tests

--- cypress/integration/testFramework.js
+++ cypress/integration/testFramework.js
@@ -9,18 +9,18 @@ describe('Hooks practice', function () {
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
   });
 
-  it('fills in form', function () {
-    cy.get('input[name="name"]:nth-child(1)').type(this.data.name);
+  it('fills form with mock data and tests form validation', function () {
+    cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
     cy.get('select').select(this.data.gender);
-
+    cy.get('input[name="name"]:nth-child(1)').should('have.value', this.data.name);    
+    cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2');  
+    cy.get('#inlineRadio3').should('be.disabled');  
   });
 
 
 
   after(function () {
     // runs once after all tests in the block
-    it('', function () {
-    });
   });
 
   beforeEach(function () {
